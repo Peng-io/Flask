274586d176d30369f89f87cb4974034fa5540a4b
Debug <1944282957@qq.com>
2022-09-04 12:03:37 +0800

    设置token以及路由守卫

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify, g
+from myjwt import *
 from flask_cors import *
 from web_sqlite import *
-import os
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, resources=r"/*")
 database = My_sqlite("studentList.db")
-app.secret_key = os.urandom(24)
 app.config['JSON_AS_ASCII'] = False
+
+
+@app.before_request
+def my_before_request():
+    token = request.headers.get("token")
+    if token:
+        if getToKen(token):
+            g.Turntable = True
+        else:
+            g.Turntable = False
+    else:
+        g.Turntable = False
 
 
 @app.route('/getStudnet', methods=["GET", "POST"])
 def hello_world():
     datalist = []
-    for i in getStudentInfo(database):
-        datalist.append(dict(zip(["id", "name", "sex", "age", "address"], i)))
-    return jsonify({"data": datalist}), 200
+    if g.Turntable:
+        for i in getStudentInfo(database):
+            datalist.append(dict(zip(["id", "name", "sex", "age", "address"], i)))
+        return jsonify({"code": True, "data": datalist}), 200
+    else:
+        return jsonify({"code": False, "msg": "请先登录"})
 
 
 @app.route("/login", methods=["POST"])
@@ -24,46 +38,56 @@ def login():
     password = request.get_json()["password"]
     print(user, password)
     if selPassword(database, user, password):
-        res = make_response({"code": True, "msg": "登录成功"})
-        res.set_cookie("user", "001", max_age=60 * 60 * 24,)
-        return res, 200
+        token = setToKen(user)
+        return jsonify({"code": True, "msg": "登录成功", "token": token}), 200
     else:
         return jsonify({"code": False, "msg": "账号密码错误"}), 200
 
 
 @app.route("/delStu", methods=["post"])
 def delStu():
-    user = request.get_json()["user"]
-    print(user)
-    if delStudent(database, user):
-        return jsonify({"code": True}), 200
+    if g.Turntable:
+        user = request.get_json()["user"]
+        print(user)
+        if delStudent(database, user):
+            return jsonify({"code": True}), 200
+        else:
+            return jsonify({"code": False}), 200
     else:
-        return jsonify({"code": False}), 200
+        return jsonify({"code": False, "msg": "请先登录"})
 
 
 @app.route("/updataStu", methods=["post"])
 def updataStu():
-    data = request.get_json()["data"]
-    upDataStudent(database, data["id"], data["name"], data["sex"], data["age"], data["address"])
-    return jsonify({"code": True}), 200
+    if g.Turntable:
+        data = request.get_json()["data"]
+        upDataStudent(database, data["id"], data["name"], data["sex"], data["age"], data["address"])
+        return jsonify({"code": True}), 200
+    else:
+        return jsonify({"code": False, "msg": "请先登录"})
 
 
 @app.route("/changePassword", methods=["post"])
 def changePassword():
-    user = request.get_json()["user"]
-    pad = request.get_json()["password"]
-    if chanGePassword(database, user, pad):
-        return jsonify({"code": True}), 200
+    if g.Turntable:
+        user = request.get_json()["user"]
+        pad = request.get_json()["password"]
+        if chanGePassword(database, user, pad):
+            return jsonify({"code": True}), 200
+        else:
+            return jsonify({"code", False}), 200
     else:
-        return jsonify({"code", False}), 200
+        return jsonify({"code": False, "msg": "请先登录"})
 
 
 @app.route("/addStu", methods=["post"])
 def addStu():
-    data = request.get_json()["data"]
-    print(data)
-    intoStudentInfo(database, data["id"], data["name"], data["sex"], data["age"], data["address"])
-    return jsonify({"code": True}), 200
+    if g.Turntable:
+        data = request.get_json()["data"]
+        intoStudentInfo(database, data["id"], data["name"], data["sex"], data["age"], data["address"])
+        return jsonify({"code": True}), 200
+    else:
+        return jsonify({"code": False, "msg": "请先登录"})
 
 
 if __name__ == '__main__':
---
+++ myjwt.py
@@ -0,0 +1,32 @@
+import jwt
+import time
+
+myKey = "ifjhasiufhsaiufnASIFUHasui"
+
+header = {
+    "typ": "JWT",
+    "alg": "HS256"
+}
+
+def MyPayLoad(user: str) -> dict:
+    return {
+        "iat": time.time(),
+        "ext": int(time.time()) + 60 * 60 * 20 * 30,
+        "user": user
+    }
+
+
+def setToKen(user: str) -> str:
+    return jwt.encode(payload=MyPayLoad(user), key=myKey, algorithm="HS256", headers=header)
+
+
+def getToKen(key: str) -> bool:
+    try:
+        data = jwt.decode(key, myKey, algorithms="HS256")
+    except:
+        return False
+    else:
+        if data.pop("ext") < time.time():
+            return False
+        else:
+            return True
