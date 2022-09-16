1e69c34541136946b4c764c24e18754911ff351a
Debug <1944282957@qq.com>
2022-09-16 23:19:17 +0800

    重构优化代码

--- 01.py
+++ 01.py
@@ -1,3 +1,14 @@
-from web_sqlite import *
+import random
+import sqlite3
 
-database = StudentList("studentList.db")
+Num = ["0003", "0004", "0005", "0006", "0007", "0008"]
+conn = sqlite3.connect("studentList.db")
+cur = conn.cursor()
+for i in Num:
+    for j in range(1, 6):
+        try:
+            sql = f"INSERT INTO grade VALUES('{i}','{j}','{random.randint(30, 100)}')"
+            cur.execute(sql)
+            conn.commit()
+        except:
+            continue
