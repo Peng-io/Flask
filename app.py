004b24da5a2b60115a31df702c0d2454c594c525
Debug <1944282957@qq.com>
2022-10-01 14:23:58 +0800

    重构优化代码

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
-from flask_cors import *
+from flask_cors import CORS
 
 from myjwt import setToKen, getToKen
 from web_sqlite import StudentList
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources=r"/*")
+CORS(app, supports_credentials=True)
 app.config["JSON_AS_ASCII"] = False
 
 
